12c8a4f5a2031813d1bda4532f14e7b4ed206f02
Ivan Chien <qyc027@gmail.com>
2022-11-25 03:04:19 +0800

    Add LuaComponent demo

---
+++ engine/source/runtime/function/framework/component/lua/lua_component.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "runtime/function/framework/component/component.h"
+#include "sol/sol.hpp"
+
+namespace Piccolo
+{
+    REFLECTION_TYPE(LuaComponent)
+    CLASS(LuaComponent : public Component, WhiteListFields)
+    {
+        REFLECTION_BODY(LuaComponent)
+
+    public:
+        LuaComponent() = default;
+
+        void postLoadResource(std::weak_ptr<GObject> parent_object) override;
+
+        void tick(float delta_time) override;
+
+    protected:
+        sol::state m_lua_state;
+
+        META(Enable)
+        std::string m_lua_script;
+
+    };
+} // namespace Piccolo
